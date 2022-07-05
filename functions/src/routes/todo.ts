e36b08944568f54889702e8ce41a61f4490b02ff
Mangrove-Sun <tjsghd0317@gmail.com>
2022-07-05 21:57:35 +0900

    feat: 'DELETE'메소드 코드 작성
    feat: 예외처리
    조회할 때 sort정렬 적용

--- functions/src/routes/todo.ts
+++ functions/src/routes/todo.ts
@@ -11,6 +11,27 @@ interface Todo {
   done: boolean
   createdAt: string
   updatedAt: string
+  deleted: boolean
+}
+
+// Job
+async function addEdeleted() {
+  const snaps = await db.collection('Todos').get()
+
+  // forEach메소드 자체가 비동기처럼 기다렸다가 넘어가지 않는다.
+  // forEach에서는 async, await가 되지 않는다.
+  // snaps.forEach(async snap => {
+  //   await snap.ref.update({
+  //     deleted: false
+  //   })
+  // })
+  // 기다렸다 넘어가게 만들기 위해 for문을 사용한다.
+  for (const snap of snaps.docs) {
+    snap.ref.update({
+      deleted: false
+    })
+  }
+  console.log('완료!')
 }
 
 // http://localhost:5001/kdt-fe2-test/us-central1/api/todo
@@ -23,7 +44,7 @@ router.get('/', async (req, res) => {
 
   // 쿼리한다 where('필드이름', '조건', '값')
   const snaps = await db.collection('Todos')
-    // .where('done', '==', false)
+    .where('deleted', '!=', true)
     .get()
 
     // 처리
@@ -35,7 +56,13 @@ router.get('/', async (req, res) => {
         ...fields as Todo
       })
     })
-  
+    
+  todos.sort((a, b) => {
+    const aTime = new Date(a.createdAt).getTime()
+    const bTime = new Date(b.createdAt).getTime()
+    return bTime - aTime
+  })
+
   res.status(200).json(todos)
 })
 
@@ -47,7 +74,8 @@ router.post('/', async (req, res) => {
     title,
     done: false,
     createdAt: date,
-    updatedAt: date
+    updatedAt: date,
+    deleted: false
   }
   const ref = await db.collection('Todos').add(todo)
 
@@ -62,6 +90,10 @@ router.put('/:id', async (req, res) => {
   const { id } = req.params
 
   const snap = await db.collection('Todos').doc(id).get()
+  // 존재하는지 않하는지
+  if (!snap.exists) {
+    return res.status(404).json('존재하지 않는 정보입니다.')
+  }
   const { createdAt } = snap.data() as Todo
   const updatedAt = new Date().toISOString()
   await snap.ref.update({
@@ -70,16 +102,40 @@ router.put('/:id', async (req, res) => {
     updatedAt
   })
 
-
-
-  res.status(200).json({
+  return res.status(200).json({
     id: snap.id,
     title,
     done,
     createdAt,
-    updatedAt
+    updatedAt,
+    deleted: false
   })
 })
-// router.delete('')
+// 투두 삭제
+// router.delete('/:id', async (req, res) => {
+//   const { id } = req.params
+
+//   const snap = await db.collection('Todos').doc(id).get()
+//   await snap.ref.delete()
+
+//   res.status(200).json(true)
+// })
+
+// 투두 삭제된 것 처럼 보이게
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const snap = await db.collection('Todos').doc(id).get()
+  // 존재하는지 않하는지
+  if (!snap.exists) {
+    return res.status(404).json('존재하지 않는 정보입니다.')
+  }
+  await snap.ref.update({
+    deleted: true
+  })
+
+  res.status(200).json(true)
+  return
+})
 
 export default router
